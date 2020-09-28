8d4884cbfeb4cef0e12aca82c1cd687eb8ac56f8
BordyV <56049773+BordyV@users.noreply.github.com>
2020-09-29 01:34:35 +0200

    avancement card tout les pays
    design card pays

--- payclient/src/components/pays.vue
+++ payclient/src/components/pays.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="petitpays">
-      <div v-if="pays">
-          <table></table>
-        <img alt="Drapeau" :src="getDrapeauPays()">
-        <h1 >{{ pays.translations.fr}}</h1>
+    <div v-if="pays">
+      <h1>{{ pays.length }}</h1>
+      <div v-for="item in pays" :key="item.name">
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">{{item.translations.fr}}</div>
+            <v-list-item-title class="headline mb-1">
+              {{item.name}}
+            </v-list-item-title>
+            <v-list-item-subtitle
+              >{{item.subregion}}</v-list-item-subtitle
+            >
+          </v-list-item-content>
+
+          <v-avatar size="80" color="grey" tile><v-img :src="item.flag"></v-img></v-avatar>
+        </v-list-item>
+      </v-card>
       </div>
+    </div>
   </div>
 </template>
 
@@ -16,17 +31,16 @@ export default {
       pays: undefined,
     };
   },
-  mounted () {
-      this.getPays();
+  mounted() {
+    this.getPays();
   },
   methods: {
-    getPays() {    
+    getPays() {
       var url = "https://restcountries.eu/rest/v2/all";
       fetch(url)
         .then((response) => {
           response.json().then((data) => {
-              
-            this.pays = data[0];
+            this.pays = data;
             console.log(this.pays);
           });
         })
@@ -35,10 +49,9 @@ export default {
           alert("Une erreur est survenue lors du chargement des données");
         });
     },
-    getDrapeauPays()
-    {
-        return this.pays.flag;
-    }
+    getDrapeauPays() {
+      return this.pays.flag;
+    },
   },
 };
 </script>
